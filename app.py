047427208daf27a62cc9c8fe52100b6f7c813594
Nishil <nishil.shah17@gmail.com>
2018-11-26 20:01:21 -0600

    adding leagues page

--- app.py
+++ app.py
@@ -6,20 +6,38 @@ access_token, uid = None, None
 
 @app.route("/")
 def index():
+    '''
+    Render index page
+    '''
     return render_template("index.html")
 
 @app.route("/home")
 def home():
+    '''
+    Render FFStats home page after user sign in
+    '''
     global access_token, uid
     return render_template("home.html", access_token=access_token, uid=uid)
 
+@app.route("/home/league/<league_id>")
+def league(league_id):
+    '''
+    Render league home page
+    '''
+    global access_token, uid
+    return render_template("league.html", access_token=access_token, uid=uid, league_id=league_id)
+
 @app.route("/signin", methods=["POST"])
 def signin():
+    '''
+    Handle google sign in with Firebase Authentication
+    '''
     data = request.data
     data = json.loads(data)
 
-    global access_token
+    global access_token, uid
     access_token = data['access_token']
+    uid = data['uid']
 
     if access_token:
         return jsonify({}), 200
